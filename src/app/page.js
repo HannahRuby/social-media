3fb7323983f0fe7a4d7c715fb94d743ad60f41a5
HANNAH <HUPEE2001@YAHOO.COM>
2024-05-18 00:50:13 +0100

    error fixing

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,11 @@ export default function Home() {
       <p>
         Welcome to Profoundly – Where Connections Transform Careers! Join our
         vibrant community of professionals from diverse industries worldwide.
-        Whether you&aposre seeking career insights, networking opportunities, or
-        mentorship, Profoundly is your gateway to meaningful connections and
+        Whether you&apos;re seeking career insights, networking opportunities,
+        or mentorship, Profoundly is your gateway to meaningful connections and
         professional growth. Connect with like-minded individuals, share
         industry insights, and unlock new opportunities to advance your career.
-        Together, let&aposs build a network that empowers and inspires success.
+        Together, let&apos;s build a network that empowers and inspires success.
         Sign up today and take the first step towards a profoundly rewarding
         career journey!
       </p>
